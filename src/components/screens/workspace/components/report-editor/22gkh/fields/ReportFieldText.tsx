c16be10002f5151238168cffafda6fc8b32c0df2
Anton Kryukov <antohakriukow@gmail.com>
2023-12-19 12:37:09 +0600

    email & phone validation

--- src/components/screens/workspace/components/report-editor/22gkh/fields/ReportFieldText.tsx
+++ src/components/screens/workspace/components/report-editor/22gkh/fields/ReportFieldText.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react'
+import { RegisterOptions } from 'react-hook-form'
 
 import { Field } from '~/components/ui'
 
+import { validEmail, validPhone } from '~/shared/regex'
 import { IReport } from '~/shared/types/report.interface'
 
 import { IReportField } from '../../report-editor.interface'
@@ -13,12 +15,23 @@ const ReportFieldText: FC<IReportField> = ({
 	isRequired,
 	error
 }) => {
+	const parameters = {} as RegisterOptions
+	if (isRequired) parameters.required = 'Обязательное поле'
+	if (fieldName?.toLowerCase().includes('email'))
+		parameters.pattern = {
+			value: validEmail,
+			message: 'Невалидный email'
+		}
+
+	if (fieldName?.toLowerCase().includes('phone'))
+		parameters.pattern = {
+			value: validPhone,
+			message: 'Невалидный номер'
+		}
+
 	return (
 		<Field
-			{...register(
-				fieldName as keyof IReport,
-				isRequired ? { required: 'Обязательное поле' } : {}
-			)}
+			{...register(fieldName as keyof IReport, parameters)}
 			error={error}
 			placeholder={placeholder}
 			type='text'
