5f378df7a357cb77e038728a0f784fd34784922d
Anton Kryukov <antohakriukow@gmail.com>
2024-03-04 06:38:58 +0500

    saving report structure type to DB on first substep removed

--- src/components/screens/annual-report/steps/initial-step/InitialStepMap.tsx
+++ src/components/screens/annual-report/steps/initial-step/InitialStepMap.tsx
@@ -29,7 +29,7 @@ const InitialStepMap = (
 	const selectStructure = {
 		stepTitle: 'Выбор структуры отчета',
 		onNext: () => {
-			handleStepOneOnNext()
+			// handleStepOneOnNext()
 			clearError()
 		},
 		component: <StructureSelector />,
