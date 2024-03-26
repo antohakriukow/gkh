7eeba749be89af601a12165fadba457ca3b5df06
Anton Kryukov <antohakriukow@gmail.com>
2024-03-26 13:58:11 +0500

    new pdf template implemented

---
+++ src/core/22gkh/pdf/report-content-templates/2023/template.ts
@@ -0,0 +1,147 @@
+// @ts-ignore
+import {
+	getRow86,
+	getRow87,
+	sectionFourTableHeader,
+	sectionFourTitle
+} from './data/sectionFour.data'
+import {
+	sectionOneNames,
+	sectionOneTableHeader,
+	sectionOneTitle
+} from './data/sectionOne.data'
+import {
+	sectionThreeNames,
+	sectionThreeTableHeader,
+	sectionThreeTitle
+} from './data/sectionThree.data'
+import {
+	sectionTwoNames,
+	sectionTwoTableHeader,
+	sectionTwoTitle
+} from './data/sectionTwo.data'
+import { INCLUDING, OF_THEM } from './data/shared.data'
+import { getSignature } from './data/signature.data'
+import { getTitleSection } from './data/title.data'
+
+import { IReport } from '~/shared/types/report.interface'
+
+import {
+	getMarginLeft,
+	getSectionThreeRow,
+	getSectionTwoRow
+} from '../../pdf.utils'
+import { getSectionOneRow } from '../../pdf.utils'
+
+export const reportTemplate2023 = (report: IReport) => [
+	...getTitleSection(report),
+	sectionOneTitle,
+	{
+		table: {
+			widths: ['*', 45, 120],
+			body: [
+				...sectionOneTableHeader,
+				[{ text: sectionOneNames.subSectionOne, bold: true }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 1),
+				...getSectionOneRow(report, sectionOneNames, 2, 2, 1),
+				[{ text: OF_THEM, ...getMarginLeft(2) }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 3, 3, 2),
+				[{ text: INCLUDING, ...getMarginLeft(3) }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 4, 10, 3),
+				...getSectionOneRow(report, sectionOneNames, 11, 11, 2),
+				[{ text: INCLUDING, ...getMarginLeft(3) }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 12, 14, 3),
+				...getSectionOneRow(report, sectionOneNames, 15, 15, 2),
+				...getSectionOneRow(report, sectionOneNames, 16),
+				...getSectionOneRow(report, sectionOneNames, 17, 17, 1),
+				[{ text: OF_THEM, ...getMarginLeft(2) }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 18, 18, 2),
+				[{ text: INCLUDING, ...getMarginLeft(3) }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 19, 25, 3),
+				...getSectionOneRow(report, sectionOneNames, 26, 26, 2),
+				[{ text: INCLUDING, ...getMarginLeft(3) }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 27, 29, 3),
+				[{ text: sectionOneNames.subSectionTwo, bold: true }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 30),
+				...getSectionOneRow(report, sectionOneNames, 31, 31, 1),
+				[{ text: sectionOneNames.subSectionThree, bold: true }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 32),
+				[{ text: INCLUDING, ...getMarginLeft(1) }, {}, {}],
+				...getSectionOneRow(report, sectionOneNames, 33, 33, 1),
+				...getSectionOneRow(report, sectionOneNames, 34, 40, 2),
+				...getSectionOneRow(report, sectionOneNames, 41, 41, 1),
+				...getSectionOneRow(report, sectionOneNames, 42, 44, 2),
+				...getSectionOneRow(report, sectionOneNames, 45),
+				...getSectionOneRow(report, sectionOneNames, 46, 47, 1),
+				...getSectionOneRow(report, sectionOneNames, 48),
+				...getSectionOneRow(report, sectionOneNames, 49, 57, 1),
+				...getSectionOneRow(report, sectionOneNames, 58, 59)
+			]
+		}
+	},
+	sectionTwoTitle,
+	{
+		table: {
+			widths: ['*', 45, 120, 120],
+			body: [
+				...sectionTwoTableHeader,
+				...getSectionTwoRow(report, sectionTwoNames, 60, 63)
+			]
+		}
+	},
+	sectionThreeTitle,
+	{
+		table: {
+			widths: ['*', 30, 50, 50, 50, 50, 90, 50],
+			body: [
+				...sectionThreeTableHeader,
+				...getSectionThreeRow(report, sectionThreeNames, 64),
+				[{ text: OF_THEM, ...getMarginLeft(1) }, {}, {}, {}, {}, {}, {}, {}],
+				...getSectionThreeRow(report, sectionThreeNames, 65, 66, 1),
+				[{ text: INCLUDING, ...getMarginLeft(2) }, {}, {}, {}, {}, {}, {}, {}],
+				...getSectionThreeRow(report, sectionThreeNames, 67, 68, 2),
+				...getSectionThreeRow(report, sectionThreeNames, 69, 69, 1),
+				...getSectionThreeRow(report, sectionThreeNames, 70),
+				...getSectionThreeRow(report, sectionThreeNames, 71),
+				...getSectionThreeRow(report, sectionThreeNames, 72, 76, 1),
+				[{ text: INCLUDING, ...getMarginLeft(2) }, {}, {}, {}, {}, {}, {}, {}],
+				...getSectionThreeRow(report, sectionThreeNames, 77, 78, 2),
+				...getSectionThreeRow(report, sectionThreeNames, 79, 80, 1),
+				...getSectionThreeRow(report, sectionThreeNames, 81, 81, 2),
+				[
+					{ text: sectionThreeNames.stoveHeatingFuel, ...getMarginLeft(1) },
+					{},
+					{},
+					{},
+					{},
+					{},
+					{},
+					{}
+				],
+				...getSectionThreeRow(report, sectionThreeNames, 82, 84, 1),
+				...getSectionThreeRow(report, sectionThreeNames, 85)
+			]
+		}
+	},
+
+	// report.period === 4 ?
+	sectionFourTitle,
+	// : {}
+	// report.period === 4 ?
+	{
+		table: {
+			widths: ['*', 45, 90, 90, 90, 90, 90],
+			body: [...sectionFourTableHeader, getRow86(report), getRow87(report)]
+		}
+	},
+
+	// : {}
+	{
+		// pageBreak: report.period === 4 ? '' : 'before',
+		margin: report.period === 4 ? [0, 50, 0, 0] : [0, 0, 0, 0],
+		table: {
+			widths: [180, 20, 190, 20, 190, 20, 78],
+			body: [...getSignature(report)]
+		}
+	}
+]
---
+++ src/core/22gkh/pdf/report-content-templates/2024/data/signature.data.ts
@@ -0,0 +1,106 @@
+import { IReport } from '~/shared/types/report.interface'
+
+import { convertTimestampToDate } from '~/utils/time.utils'
+
+export const getSignature = (report: IReport) => [
+	[
+		{
+			text: 'Должностное лицо, ответственное за предоставление статистической информации (лицо, уполномоченное предоставлять статистическую информацию от имени юридического лица)',
+			border: [false, false, false, false],
+			fontSize: 10
+		},
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: report.company.leader_post,
+			alignment: 'center',
+			border: [false, false, false, true],
+			margin: [0, 60, 0, 0]
+		},
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: report.company.leader_name,
+			alignment: 'center',
+			border: [false, false, false, true],
+			margin: [0, 60, 0, 0]
+		},
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: '',
+			alignment: 'center',
+			border: [false, false, false, true]
+		}
+	],
+
+	[
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: '(должность)',
+			fontSize: 10,
+			alignment: 'center',
+			border: [false, false, false, false]
+		},
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: '(Ф.И.О.)',
+			fontSize: 10,
+			alignment: 'center',
+			border: [false, false, false, false]
+		},
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: '(подпись)',
+			fontSize: 10,
+			alignment: 'center',
+			border: [false, false, false, false]
+		}
+	],
+
+	[
+		{ text: '', border: [false, false, false, false], margin: [0, 20, 0, 0] },
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] }
+	],
+
+	[
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: report.company.phone,
+			alignment: 'center',
+			border: [false, false, false, true]
+		},
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: convertTimestampToDate(+report.updatedAt),
+			alignment: 'center',
+			border: [false, false, false, true]
+		},
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] }
+	],
+
+	[
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: 'Номер контактного телефона',
+			border: [false, false, false, false],
+			fontSize: 10,
+			alignment: 'center'
+		},
+		{ text: '', border: [false, false, false, false] },
+		{
+			text: 'Дата составления документа',
+			border: [false, false, false, false],
+			fontSize: 10,
+			alignment: 'center'
+		},
+		{ text: '', border: [false, false, false, false] },
+		{ text: '', border: [false, false, false, false] }
+	]
+]
